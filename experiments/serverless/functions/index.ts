afa986614c8b733a1c222cde25ac18e52b129050
Dzmitry Bartalevich <dimati.bart@gmail.com>
2020-07-14 13:13:19 +0300

    added simple backend feature toggle

--- experiments/serverless/functions/index.ts
+++ experiments/serverless/functions/index.ts
@@ -1,7 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import {getResponse} from "../helpers";
+import {getFeatureToggle, FeatureToggleClientInterface} from "../../../src/FeatureToggle";
+const SDK_KEY = process.env.SDK_KEY;
 
-module.exports.handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler =  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log(event);
-    return getResponse(200, {message: "H~e"})
+
+    if (!SDK_KEY) {
+        throw new Error("No SDK key provided");
+    }
+
+    const featureToggleClient: FeatureToggleClientInterface = await getFeatureToggle(SDK_KEY);
+    if (featureToggleClient.isFeatureEnabled("test_feature", "3456798765")) {
+        return getResponse(200, {message: "Feature is on!"})
+    }
+    else {
+        return getResponse(200, {message: "Feature is off!"})
+    }
 };
