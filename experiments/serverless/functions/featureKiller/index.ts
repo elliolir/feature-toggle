3d99420ed750172ecc0c7e09770fce367aec4b70
Dmitry Beloichuk <dmitry_beloichuk@epam.com>
2020-07-20 18:05:23 +0300

    Add getFeatureIdFromKey utility function

--- experiments/serverless/functions/featureKiller/index.ts
+++ experiments/serverless/functions/featureKiller/index.ts
@@ -1,9 +1,11 @@
 import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
 import { getResponse } from '../../helpers';
+import getFeatureIdFromKey from '../../utils/getFeatureIdFromKey';
+import { IFeature } from '../../utils/interfaces';
 
 import { patchFeature } from './helpers';
-import { IFeatureKiller, IFeatureKillerResult } from './interface';
+import { IFeatureKiller } from './interface';
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
   console.log('Body: ', event.body);
@@ -11,7 +13,10 @@ export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyRe
   const { featureId }: IFeatureKiller = JSON.parse(event.body);
   console.log('Feature ID: ', featureId);
 
-  const data: IFeatureKillerResult = await patchFeature(featureId);
+  const feature = await getFeatureIdFromKey('home_route');
+  console.log('Feature: ', feature);
+
+  const data: IFeature = await patchFeature(featureId);
 
   return getResponse(200, data);
 };
--- experiments/serverless/functions/featureKiller/interface.ts
+++ experiments/serverless/functions/featureKiller/interface.ts
@@ -1,43 +1,11 @@
+import { IEnvironments } from '../../utils/interfaces';
+
 interface IFeatureKiller {
   featureId: number;
-}
-
-interface IRolloutRule {
-  audience_conditions: string;
-  enabled?: boolean;
-  percentage_included?: number;
-}
-
-interface IEnvironments {
-  development: {
-    id?: number;
-    is_primary?: boolean;
-    rollout_rules: IRolloutRule[];
-  };
-  production: {
-    id?: number;
-    is_primary?: boolean;
-    rollout_rules: IRolloutRule[];
-  };
 }
 
 interface IPayload {
   environments: IEnvironments;
 }
 
-interface IFeatureKillerResult {
-  id: number;
-  key: string;
-  name: string;
-  description: string;
-  project_id: number;
-  environments: IEnvironments;
-  variables: Array<{
-    [key: string]: string;
-  }>;
-  archived: boolean;
-  created: string;
-  last_modified: string;
-}
-
-export { IFeatureKiller, IFeatureKillerResult, IPayload };
+export { IFeatureKiller, IPayload };
